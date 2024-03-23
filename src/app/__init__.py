5a9482cb8eca26e91d61eff6f0c961844672c98f
rlaqudgus <108641304+rlaqudgus@users.noreply.github.com>
2024-03-23 19:48:31 +0900

    register
    회원가입

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 JSON_FILE = "posts.json"
 socketio = SocketIO(app)
 
-from app import login, gallery
+from app import login, gallery,socket
--- src/app/data.py
+++ src/app/data.py
@@ -16,10 +16,36 @@ def save_data(data):
     with open(JSON_FILE, 'w') as file:
         json.dump(data, file, indent=4)
 
-def create_post(title, content):
+def register_user(username, id, password):
+    data = load_data()
+
+    #사용자 정보 확인
+    for user in data["users"]:
+        if user["username"] == username:
+            return False, "이미 사용 중인 사용자 이름입니다."
+        if user["id"] == id:
+            return False, "이미 사용 중인 아이디입니다."
+        if user["password"] == password:
+            return False, "이미 사용 중인 비밀번호입니다."
+    
+     # 사용자 정보 저장
+    new_user = {
+        "username": username,
+        "id": id,
+        "password": password  # 비밀번호 저장 전에 해싱 또는 암호화 필요
+    }
+    data["users"].append(new_user)
+    save_data(data)
+    
+    return True, "회원가입이 완료되었습니다."
+    
+
+
+def create_post(username, title, content):
     data = load_data()
     new_post = {
-        "id": len(data["posts"]) + 1,
+        "num": len(data["posts"]) + 1,
+        "username":username,
         "title": title,
         "content": content,
         "date_posted": datetime.utcnow().isoformat(),
@@ -29,10 +55,11 @@ def create_post(title, content):
     save_data(data)
     return new_post
 
-def add_comment(post_id, content):
+def add_comment(username, post_id, content):
     data = load_data()
     comment = {
-        "id": len(data["comments"]) + 1,
+        "num": len(data["comments"]) + 1,
+        "username": username,
         "post_id": post_id,
         "content": content,
         "date_commented": datetime.utcnow().isoformat()
